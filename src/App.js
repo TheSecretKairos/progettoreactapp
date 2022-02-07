3536e2977fd4fb584c3078f2dadee9f0771a6e9f
Philippe Guarino <P.Guarino@leedsbeckett.ac.uk>
2022-02-07 16:26:59 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and salva to reload.
+          Edit <code>src/App.js</code> and salva q2 to reload.
         </p>
         <a
           className="App-link"
